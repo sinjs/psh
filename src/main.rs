b1184fe99b2f5f33bb09df012a6cfadbed112812
sinmineryt <50749166+sinmineryt@users.noreply.github.com>
2023-04-09 23:52:26 +0200

    add basic startup logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
+use std::env;
+use std::io;
+
+const NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+#[cfg(debug_assertions)]
+const TARGET: &str = "dev";
+#[cfg(not(debug_assertions))]
+const TARGET: &str = "release";
+
+fn parse_command(input: String) -> Option<String> {
+    let trimmed = input.trim().to_string();
+    if trimmed.len() == 0 {
+        return None;
+    }
+    Some(trimmed)
+}
+
+fn read_line() -> Result<String, io::Error> {
+    let mut buffer = String::new();
+    match io::stdin().read_line(&mut buffer) {
+        Ok(_) => Ok(buffer),
+        Err(err) => Err(err),
+    }
+}
+
+fn parse_args(command: String) -> Vec<String> {
+    command.split_whitespace().map(|s| s.to_string()).collect()
+}
+
+fn parse_shell_args() -> Result<Vec<String>, ()> {
+    let mut shell_args: Vec<String> = env::args().collect();
+    shell_args.reverse();
+    shell_args.pop();
+    shell_args.reverse();
+
+    Ok(shell_args)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut shell_args = parse_shell_args().expect("Failed to parse arguments");
+    let mut shell_args_temp = shell_args.clone();
+
+    for (index, argument) in shell_args.iter_mut().enumerate() {
+        if argument == &String::from("--no-greeting") {
+            // TODO: support config(custom-shell-greeting)
+            //               args(custom-shell-greeting)
+            println!("welcome to {} v{}-{}", NAME, VERSION, TARGET);
+            shell_args_temp.remove(index);
+        }
+    }
+
+    let shell_args = shell_args_temp;
+
+    dbg!(shell_args);
+
+    loop {
+        let command = parse_command(read_line().unwrap());
+
+        let command = match command {
+            None => continue,
+            Some(cmd) => cmd,
+        };
+
+        let args = parse_args(command.clone());
+
+        dbg!(command, args);
+    }
 }
