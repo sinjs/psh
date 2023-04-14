ee9f8f7a9c3eb873cda3d5cb3a9598a1906f5a6e
Aiden <50749166+sinjs@users.noreply.github.com>
2023-04-14 15:34:10 +0200

    config: allow for reading from .pshrc file

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,16 @@
+use crate::constants;
+use colored::*;
+use ini::Ini;
+use regex::Regex;
+use std::collections::HashMap;
 use std::env;
+use std::io;
+use std::path::Path;
+
+type FileConfig<'group_key> = HashMap<&'group_key str, HashMap<String, String>>;
 
 pub fn create_config_object() -> ConfigManager {
+    read_pshrc_to_map();
     let mut argv: Vec<String> = env::args().collect();
     let mut data = ConfigData {
         debug: false,
@@ -21,6 +31,109 @@ pub fn create_config_object() -> ConfigManager {
     }
 
     ConfigManager { data }
+}
+
+#[cfg(windows)]
+pub fn expand_env_vars(s: &str) -> std::io::Result<String> {
+    lazy_static! {
+        static ref ENV_VAR: Regex = Regex::new("%([[:word:]]*)%").expect("Regex is invalid");
+    }
+
+    let result: String = ENV_VAR
+        .replace_all(s, |captures: &regex::Captures| match &captures[1] {
+            "" => String::from("%"),
+            varname => env::var(varname).expect("Environment variable is invalid"),
+        })
+        .into();
+
+    Ok(result)
+}
+
+#[cfg(not(windows))]
+pub fn expand_tilde(s: &str) -> String {
+    shellexpand::tilde(s).into()
+}
+
+pub fn read_pshrc_to_map<'group_key>() -> FileConfig<'group_key> {
+    let mut path: Option<String> = None;
+    let mut map: FileConfig = HashMap::new();
+
+    for location in constants::CONFIG_LOCATIONS {
+        #[cfg(windows)]
+        let location = expand_env_vars(location).unwrap();
+
+        #[cfg(not(windows))]
+        let location = expand_tilde(location);
+
+        match Path::new(&location).try_exists() {
+            Ok(exists) => match exists {
+                true => {
+                    path = Some(location.to_string());
+                    break;
+                }
+                false => {
+                    if cfg!(debug_assertions) {
+                        dbg!(location);
+                    }
+                }
+            },
+            Err(e) => match e.kind() {
+                io::ErrorKind::PermissionDenied => continue,
+                _ => {
+                    eprintln!(
+                        "pshrc: {} reading {}: {}",
+                        "error:".bold().bright_red(),
+                        location,
+                        e
+                    );
+                }
+            },
+        }
+    }
+
+    if let Some(path) = path {
+        let config_parsed = Ini::load_from_file(path).unwrap();
+        for (selection, property) in config_parsed.iter() {
+            match selection {
+                Some(selection) => match selection.to_lowercase().as_str() {
+                    "flags" => {
+                        let mut property_map: HashMap<String, String> = HashMap::new();
+
+                        for (key, value) in property.iter() {
+                            property_map.insert(key.to_string(), value.to_string());
+                        }
+
+                        map.insert("flags", property_map);
+                    }
+                    _ => {
+                        for (key, value) in property.iter() {
+                            eprintln!(
+                                "pshrc: {} ignoring property in invalid group {}: {}={}",
+                                "warn:".bold().yellow(),
+                                selection.bold(),
+                                key,
+                                value
+                            );
+                        }
+                    }
+                },
+                None => {
+                    for (key, value) in property.iter() {
+                        eprintln!(
+                            "pshrc: {} ignoring property without group: {}={}",
+                            "warn:".bold().yellow(),
+                            key,
+                            value
+                        );
+                    }
+                }
+            }
+        }
+    };
+
+    dbg!(&map);
+
+    map
 }
 
 #[derive(Debug)]
--- src/constants.rs
+++ src/constants.rs
@@ -4,3 +4,19 @@ pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const TARGET: &str = "dev";
 #[cfg(not(debug_assertions))]
 pub const TARGET: &str = "release";
+
+// psh configuration locations, specified in the order that they are prioritized
+// warning: platform specific, please modify when porting to other platforms
+#[cfg(target_os = "windows")]
+pub const CONFIG_LOCATIONS: &[&str] = &[
+    "C:\\.pshrc",
+    "%USERPROFILE%\\.pshrc",
+    "%USERPROFILE%\\.config\\.pshrc",
+];
+
+#[cfg(unix)]
+pub const CONFIG_LOCATIONS: &[&str] = &["/.pshrc", "/etc/pshrc", "~/.pshrc", "~/.config/.pshrc"];
+
+#[cfg(target_os = "pene")]
+pub const CONFIG_LOCATIONS: &[&str] =
+    &["/.pshrc", "/etc/pshrc", "~/.pshrc", "~/.config/PeneShellRC"];
